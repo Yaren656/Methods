f38861e98dc9718dcb287ca01e932871ba06af85
Yaren Yuvka <yarenyuvka@gmail.com>
2021-01-17 19:37:03 +0300

    Add project files.

---
+++ Methods/SepetManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar_1
+{
+    class SepetManager
+    {
+        //naming convention
+        //syntax
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Tebrikler.Sepete eklendi! : " + urun.Adi);
+        }
+
+        public void Ekle2(string urunAdi, string aciklama, double fiyat, int stokAdedi)
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi :" + urunAdi);
+        }
+
+    }
+}
---
+++ Methods/Urun.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar_1
+{
+    class Urun
+    {
+        //Property - özellik 
+        public int Id { get; set; }
+        public string Adi { get; set; }
+        public double Fiyati { get; set; }
+        public string Aciklama { get; set; }
+        public string StokAdedi { get; set; }
+
+    }
+}
